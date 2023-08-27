850aa4391382b4f4d3c8fd5a0bef7a574a3b6756
nfldty <57706682+nfldty@users.noreply.github.com>
2023-08-27 05:26:22 -0400

    modifying all files to the latest file

--- app.py
+++ app.py
@@ -10,11 +10,21 @@ import speech_recognition as sr
 import pyaudio
 import wave
 import random
+from diffusers import StableDiffusionPipeline, EulerDiscreteScheduler
+import torch 
+from generated import generated_data
+'''
+pipe = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4")
+pipe.to("cuda")
+prompt = "a photograph of an astronaut riding a horse"
 
-co = cohere.Client(config.api_key)
+image = pipe(prompt).images[0]
+'''
 
 
-words = ["accept", "meaning", "school", "total", "view", "degree", "who", 'where', 'why', 'interesting']
+
+words = generated_data.challenging_words.copy()
+sentences = generated_data.stories.copy()
 
 #image = Image.open('image.png', mode='r')
 #print(image_to_string(image))
@@ -74,10 +84,11 @@ def upload():
         for i in range(len(d["conf"])):
             print([d[key][i] for key in d])  
         
-        summary = co.summarize(text=raw).summary
-        print(summary)
-        print(type(summary))
-        return render_template('upload.html',img_shape=img_shape, d=d, font_size=text_size, data_size=data_size, converted_text=converted_text, img_url=raw_file_location, filename=generated_filename, summary=summary)
+        #summary = co.summarize(text=raw).summary
+        img_src = url_for('static', filename=f'saved_img/{generated_filename}')
+       
+        print(generated_filename)
+        return render_template('upload.html', img_src=img_src, img_shape=img_shape, d=d, font_size=text_size, data_size=data_size, converted_text=converted_text, img_url=raw_file_location, filename=generated_filename)
     
     else:
         
@@ -92,14 +103,17 @@ def about():
 def converted():
     return render_template('features.html')
 
+global words_idx
+words_idx = random.randint(0, 4)
 global word
-word = random.choice(words)
+word = words[words_idx]
 score = 0
 
 @app.route('/learn',  methods=['POST', 'GET'])
 def learn():
-    global word
+    global words_idx
     global score
+    global word
     
     message=""
     if request.method == 'POST':
@@ -107,24 +121,28 @@ def learn():
         if text == word:
             message = "Well done!"
             old_word = word
-            word = random.choice(words)
+            words_idx = random.randint(0, 4)
+            word = words[words_idx]
             score += 1
-            return render_template("correct.html", message=message, word=old_word, written=text, score=score)
+            return render_template("correct.html", message=message, words_idx=words_idx, word=old_word, written=text, score=score)
         else:
             message = "Try again"
-            return render_template("learn.html", message=message, word=word, written=text, len_written=len(text), len_word=len(word))
+            return render_template("learn.html", words_idx=words_idx, message=message, word=word, written=text, len_written=len(text), len_word=len(word))
         
-    return render_template("learn.html", message=message, word=word)
+    return render_template("learn.html", message=message, word=word, words_idx=str(words_idx))
 
 
 
-speechs = ["i love kimberly so much", "kimberly is my girlfriend", "my name is kimberly"]
+speechs = ["i love kimberly so much", "kimberly is my girlfriend", "my name is kimberly", "my name is john", "my name is kevin", "my name is adam", "my name is tommy", "my name is ben", 'ben', 'john', 'tommy', 'alpha'] * 100
 
 global speech
-speech = random.choice(speechs)
 
+co = cohere.Client(config.api_key)
 
-
+global speech_idx
+speech_idx = random.randint(0, 4)
+global speech
+speech=sentences[speech_idx]
 
 
 @app.route('/finished', methods=['POST', 'GET'])
@@ -137,10 +155,17 @@ def done_learn():
 @app.route('/audio', methods=['POST', 'GET'])
 def audio():
     global speech
-    
-    speechs.remove(speech)
-    
-    speech = random.choice(speechs)
+    '''
+
+    model_id = "stabilityai/stable-diffusion-2"
+    scheduler = EulerDiscreteScheduler.from_pretrained(model_id, subfolder="scheduler")
+    pipe = StableDiffusionPipeline.from_pretrained(model_id, scheduler=scheduler, torch_dtype=torch.float16)
+    pipe = pipe.to("cuda")
+
+    prompt = speech
+    image = pipe(prompt).images[0]
+    image.save('hug.jpg')
+    '''
     return render_template('recording.html', speech=speech)
 
 global false_words
@@ -151,7 +176,7 @@ false_words = []
 def record():
     global speech
     global false_words
-    speech_words = speech.replace('.', '').replace(',', '').lower().split()
+    speech_words = speech.replace('.', '').replace(',', '').replace(':','').replace("'","").lower().split()
     print(speech_words)
     print('start')
     r = sr.Recognizer()
@@ -165,22 +190,34 @@ def record():
         except: 
             text = "Sorry, We couldn't hear you"
     text_words = text.lower().split()
+    print(text_words)
     correct = 0
     total = len(speech_words)
     incorrect_words = []
     for word in speech_words:
         if word in text_words:
-            text_words.remove(word)
             correct+=1
         else:
+            
             incorrect_words.append(word)
     false_words = incorrect_words
     if len(false_words) == 0:
-        first_try = "yes"
+        global speech_idx
+        return redirect('/first_try')
     else:
         first_try = "no"
-    return render_template("recording.html", speech=speech, text=text, score=correct/total, incorrect=incorrect_words,first_try=first_try)
+    old_speech = speech
+    global speech_idx
+    speech_idx = random.randint(0, 4)
+    speech=sentences[speech_idx]
+    return render_template("recording.html", speech=old_speech, text=text, score=round(correct/total*100), incorrect=incorrect_words,first_try=first_try)
 
+@app.route('/first_try') 
+def first_try():
+    global speech_idx
+    old_idx = speech_idx
+    speech_idx = random.randint(0, 4)
+    return render_template("first_try.html", filename=f'generated_image/story_{old_idx}.jpg')
 
 
 cur_word = ''
@@ -189,7 +226,6 @@ cur_word = ''
 def read_display():
     global cur_word
     global false_words
-    print(false_words)
     cur_word = false_words.pop()
     return render_template("speech.html", word=cur_word)
 
@@ -212,20 +248,35 @@ def read_start():
             text = r.recognize_google(audio)
         except: 
             text = "Sorry, We couldn't hear you"
+    
+    with open('static/recorded_sound/recorder.wav', "wb") as f:
+            f.write(audio.get_wav_data())
+    print('false_words before detected')
+    print(false_words)
     text_words = text.lower().split()
     correct = 0
     if speech_word.lower() in text_words:
         correct="true"
+        print('true')
     else:
         correct="false"
-        false_words.append(speech_word)
+        print('appending')
+        if speech_word not in false_words:
+            false_words.append(speech_word)
+    
     
     if len(false_words) == 0:
+        
         return redirect("/finish-read")
     
-    return render_template("speech.html", speech=speech, text=text, correct=correct, single_word="true")
+    return render_template("speech.html", speech=speech, word=speech_word, text=text, correct=correct, single_word="true")
 
 
 @app.route("/finish-read")  
 def finish_read():
-    return render_template("finishread.html")
+    global speech_idx
+    global words
+    old_idx = speech_idx
+    speech_idx = random.randint(0, 4)
+    return render_template("finishread.html", filename=f'generated_image/story_{old_idx}.jpg', caption=sentences[old_idx])
+
