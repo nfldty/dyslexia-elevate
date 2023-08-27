99010b2ad28db9df64939b2969179d7fe8408f10
nfldty <57706682+nfldty@users.noreply.github.com>
2023-08-27 05:25:47 -0400

    modifying all files to newest file

---
+++ generated/generated_data.py
@@ -0,0 +1,10 @@
+challenging_words = [
+    "people", "restaurant", "intelligence", "bicycle", "success"]
+
+stories = [
+    "Forgotten key unlocked door to enchanted realm.",
+    "Haunted mirror reflected true self, broke enchantment forever.",
+    "Footprints in sand: visitor from stars left cosmic message.",
+    "Ocean whispers guided shipwrecked sailor to hidden island.",
+    "Butterfly journey unveiled parallel world of vibrant colors."
+]
